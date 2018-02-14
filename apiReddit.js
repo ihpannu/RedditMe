6e0e9faae5efebd6485f105e7ef4c1eaf232678d
iharmanpannu <iharmanpannu@gmail.com>
2018-02-14 14:35:42 -0800

    changes

--- apiReddit.js
+++ apiReddit.js
@@ -1,40 +1,10 @@
 export default {
   search: function(searchTerm, searchLimit, sortBy) {
     return fetch(
-      `https://cors-anywhere.herokuapp.com/http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit} `
+      `http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit} `
     )
       .then(res => res.json())
       .then(data => data.data.children.map(data => data.data))
       .catch(err => console.log(err));
   }
 };
-
-// ("use strict");
-
-// Object.defineProperty(exports, "__esModule", {
-//   value: true
-// });
-// exports.default = {
-//   search: function search(searchTerm, searchLimit, sortBy) {
-//     return fetch(
-//       "http://www.reddit.com/search.json?q=" +
-//         searchTerm +
-//         "&sort=" +
-//         sortBy +
-//         "&limit=" +
-//         searchLimit +
-//         " "
-//     )
-//       .then(function(res) {
-//         return res.json();
-//       })
-//       .then(function(data) {
-//         return data.data.children.map(function(data) {
-//           return data.data;
-//         });
-//       })
-//       .catch(function(err) {
-//         return console.log(err);
-//       });
-//   }
-// };
--- dist/3872dff7c6f2c85afe6b165d63c5434a.js
+++ dist/3872dff7c6f2c85afe6b165d63c5434a.js
@@ -69,7 +69,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({18:[function(require,module,exports) {
+})({7:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -100,7 +100,7 @@ function getBaseURL(url) {
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
 
-},{}],16:[function(require,module,exports) {
+},{}],5:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -132,13 +132,13 @@ function reloadCSS() {
 
 module.exports = reloadCSS;
 
-},{"./bundle-url":18}],14:[function(require,module,exports) {
+},{"./bundle-url":7}],3:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"./../img/background.jpg":15,"_css_loader":16}],20:[function(require,module,exports) {
+},{"./../img/background.jpg":6,"_css_loader":5}],8:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -158,7 +158,7 @@ module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
-  var ws = new WebSocket('ws://' + hostname + ':' + '58991' + '/');
+  var ws = new WebSocket('ws://' + hostname + ':' + '59740' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -259,7 +259,7 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}],18:[function(require,module,exports) {
+},{}],7:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -290,7 +290,7 @@ function getBaseURL(url) {
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
 
-},{}],21:[function(require,module,exports) {
+},{}],10:[function(require,module,exports) {
 var getBundleURL = require('./bundle-url').getBundleURL;
 
 function loadBundlesLazy(bundles) {
@@ -369,7 +369,7 @@ LazyPromise.prototype.catch = function (onError) {
   return this.promise || (this.promise = new Promise(this.executor).catch(onError));
 };
 
-},{"./bundle-url":18}],0:[function(require,module,exports) {
-var b=require(21);b.load([["9f373d8e36ce257fda73db5cbcb6033f.jpg",15]]);
-},{}]},{},[20,0])
+},{"./bundle-url":7}],0:[function(require,module,exports) {
+var b=require(10);b.load([["9f373d8e36ce257fda73db5cbcb6033f.jpg",6]]);
+},{}]},{},[8,0])
 //# sourceMappingURL=/dist/3872dff7c6f2c85afe6b165d63c5434a.map
